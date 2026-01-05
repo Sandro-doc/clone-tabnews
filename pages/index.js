d1b5d284b429cc88928a2a2749e320ccdc3fecb1
Sandro Rosa <sandro_rosa_oliveira@outlook.com>
2026-01-05 04:20:47 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-    return <h1>O amor tudo sofre, tudo crê, tudo espera, tudo suporta. </h1>
+  return <h1>O amor tudo sofre, tudo crê, tudo espera, tudo suporta. </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
-
