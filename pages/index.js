e48e276fddd7033a8682c249d720171a08ffc21f
Sandro Rosa <sandro_rosa_oliveira@outlook.com>
2026-02-11 10:23:56 -0300

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>O amor tudo sofre, tudo crê, tudo espera, tudo suporta. </h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
